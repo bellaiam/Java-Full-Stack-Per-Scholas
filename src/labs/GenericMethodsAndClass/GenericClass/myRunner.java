b35c84b06d1ac3b604d19352699c34815e1a3148
Bella <bellaiam@uw.edu>
2022-12-08 10:44:08 -0800

    update

--- src/labs/GenericMethodsAndClass/GenericClass/myRunner.java
+++ src/labs/GenericMethodsAndClass/GenericClass/myRunner.java
@@ -5,14 +5,14 @@ public class myRunner {
         // initialize generic class
         // with String and Integer data
 
-        GMultipleDatatype<String, Integer> mobj = new GMultipleDatatype("Per Scholas", 11025);
+        GMultipleDatatype<String, Integer> mobj = new GMultipleDatatype<>("Per Scholas", 11025);
 
         System.out.println(mobj.getValueOne());
         System.out.println(mobj.getValueTwo());
 
         // initialize generic class
         // with String and String data
-        GMultipleDatatype<String, String> mobj2 = new GMultipleDatatype("Per Scholas", "Non profit");
+        GMultipleDatatype<String, String> mobj2 = new GMultipleDatatype<>("Per Scholas", "Non profit");
         System.out.println(mobj2.getValueOne());
         System.out.println(mobj2.getValueTwo());
     }
